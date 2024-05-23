b813e840dd4834e567a0d5792f379013307e0b9b
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2024-05-23 08:54:30 -0300

    create web editor types

--- src/providers/webEditor/webEditor-provider.tsx
+++ src/providers/webEditor/webEditor-provider.tsx
@@ -1,2 +1,21 @@
+import React from 'react'
+import { WebEditorButtons } from '@/lib/constants'
 
 export type DeviceTypes = 'Desktop' | 'Mobile' | 'Tablet'
+
+export type WebEditorElement = {
+	id: string
+	styles: React.CSSProperties
+	name: string
+	type: WebEditorButtons
+	content: WebEditorElement[] | { href?: string; innerText?: string; src?: string }
+}
+
+export type WebEditor = {
+	liveMode: boolean
+	elements: WebEditorElement[]
+	selectedElement: WebEditorElement
+	device: DeviceTypes
+	previewMode: boolean
+	funnelPageId: string
+}
